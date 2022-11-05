4b7792db177c8d92fa61d37914602c7cef3c630a
Pomba Magar <11157420+pombadev@users.noreply.github.com>
2022-11-05 09:31:42 +0545

    apply black format, remove unused stuffs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 import os
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
@@ -6,13 +5,9 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-
-logger = logging.getLogger(__name__)
 
 
 class KillOnClick(Extension):
-
     def __init__(self):
         super(KillOnClick, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
@@ -20,23 +15,24 @@ class KillOnClick(Extension):
 
 
 class KeywordQueryEventListener(EventListener):
-
     def on_event(self, event, extension):
         items = []
-        items.append(ExtensionResultItem(icon='images/icon.png',
-                                         name="Kill window",
-                                         on_enter=ExtensionCustomAction("kill", keep_app_open=False)))
+        items.append(
+            ExtensionResultItem(
+                icon="images/icon.png",
+                name="Kill window",
+                on_enter=ExtensionCustomAction("kill", keep_app_open=False),
+            )
+        )
 
         return RenderResultListAction(items)
 
 
 class ItemEnterEventListener(EventListener):
-
     def on_event(self, event, extension):
 
-        ret = os.system(
-            f"bash -c 'xkill'")
+        os.system(f"bash -c 'xkill'")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     KillOnClick().run()
